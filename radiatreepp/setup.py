e779077cb5335b831c6eee2194cbf2651cb9b4e3
Soltani Kazemi <esdft@meru2.rnet.missouri.edu>
2025-07-03 01:04:30 -0500

    Update setup

--- radiatreepp/setup.py
+++ radiatreepp/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+'''from setuptools import setup, find_packages
 import os
 
 setup(
@@ -18,4 +18,30 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
     ],
+)'''
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='radiatreepp',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy',
+        'matplotlib',
+    ],
+    include_package_data=True,
+    author='Elham Soltani Kazemi',
+    description='Radial dendrogram visualization for hierarchical clustering with feature importance overlays',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/es15326/radialtreepp.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
 )
+
